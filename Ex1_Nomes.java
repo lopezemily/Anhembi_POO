85475bcd52ab960eed3ceac6481fd35088f5319d
Emily Lopez <emiily.jessica@hotmail.com>
2018-09-02 18:56:01 -0300

    Renomeando classes

---
+++ Ex1_Nomes.java
@@ -0,0 +1,13 @@
+public class Ex1_Nome {
+
+    public static void main(String[] args) {
+        Nome("João", 30, "Coringão");
+    }
+    
+    static void Nome(String nome,  int idade, String time){
+        System.out.println("***************");
+        System.out.println(nome + ", " + idade);
+        System.out.println(time);
+        System.out.println("***************");
+    }
+}
