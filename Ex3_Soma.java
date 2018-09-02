75b98b1f70016abaec25e869c353579005f1bad1
Emily Lopez <emiily.jessica@hotmail.com>
2018-09-02 19:08:16 -0300

    Adicionado exercicio soma

---
+++ Ex3_Soma.java
@@ -0,0 +1,10 @@
+public class Ex3_Soma {
+
+    public static void main(String[] args) {
+        System.out.println(SomaNum(50, 100));
+    }
+    
+    public static int SomaNum(int num1, int num2){
+        return num1 + num2;
+    }
+}
