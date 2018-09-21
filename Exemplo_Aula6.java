badd9684be49d1047155ee32a8778efdfbf2b438
Emily Lopez <38917856+lopezemily@users.noreply.github.com>
2018-09-20 21:21:27 -0300

    Add files via upload

---
+++ Exemplo_Aula6.java
@@ -0,0 +1,14 @@
+package exemplo_aula6;
+
+public class Exemplo_Aula6 {
+
+    public static void main(String[] args) {
+        String cidades [] = {"São Paulo", "Marilia", "Assis","São José dos Campos", "Santo André", "Suzano", "Araçatuba", "Penápoles", "Bauru"};
+        
+        for(String cidade:cidades){
+            System.out.println(cidade);
+        }
+        
+    }
+    
+}
